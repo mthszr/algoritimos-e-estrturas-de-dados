4e1ec642c1d266a701bc2b2bfc48e3ec70f01aa6
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-09-21 07:23:27 -0300

    final version, now it uses dsu

--- Lista 4 - Grafos/aMAZEing.cpp
+++ Lista 4 - Grafos/aMAZEing.cpp
@@ -1,45 +1,102 @@
-// https://iudex.io/problem/5bcc95305cd44b0001fdbf74
+#include <iostream>
 
-#include<stdio.h>
-#include<algorithm> // swap
+using namespace std;
 
-int t,n,m,w,q,x,y,linha,num,par[10005],sz[10005];
+const int MAX_N = 100;                // tamanho máximo do labirinto
+const int MAX_CELLS = MAX_N * MAX_N;  // número máximo de células no labirinto
 
-int find(int a) {
-    if (par[a] == a) return a;
-    return par[a] = find(par[a]);
-}
+// estrutura de dados para o Disjoint Set Union (DSU)
+class DSU {
+   private:
+    int parent[MAX_CELLS], rank[MAX_CELLS];
 
-void join(int a, int b) {
-    a = find(a), b = find(b);
-    if (a == b) return;
-    if (sz[a] < sz[b]) std::swap(a,b);
-    sz[a] += sz[b];
-    par[b] = a;
-}
-
-int main()
-{
-    scanf("%d",&t);
-
-    for (int caso = 0; caso < t; caso++) {
-        
-        scanf("%d %d %d", &n,&m,&q);
-
-        for (int i = 0; i < n*n; i++) par[i] = i, sz[i] = 1;
-        
-        for (int i = 0; i < m; i++) {
-            scanf("%d",&w);
-            linha = w / (2*n - 1); // 2n-1 paredes por linha
-            num = w % (2*n - 1); // para uma dada linha, numerando colunas de 0 a 2n-2. as primeiras n-1 sÃ£o verticais.
-            if (num < n-1) join(linha*n + num, linha*n + num + 1); // parede vertical: junta a com a+1
-            else join(linha*n + num-(n-1), linha*n + n + num-(n-1)); // parede horizontal: junta a com a+n
+   public:
+    DSU(int n) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;  // inicializa cada elemento como seu próprio pai
+            rank[i] = 0;    // inicializa o rank como 0
         }
-        
-        for (int i = 0; i < q; i++){
-            scanf("%d %d", &x, &y);
-            printf("%d.%d %d\n",caso,i,find(x) == find(y));
-        }        
-        printf("\n");
-    } 
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);  // compressão de caminho
+        }
+        return parent[x];
+    }
+
+    void union_sets(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            // união por rank
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int k;  // número de casos de teste
+    cin >> k;
+
+    // loop sobre cada caso de teste
+    for (int caso = 0; caso < k; caso++) {
+        int n, m, q;  // n = tamanho do labirinto, m = número de paredes removidas, q = número de consultas
+        cin >> n >> m >> q;
+
+        int total_cells = n * n;  // calcula o total de células no labirinto
+
+        DSU dsu(total_cells);  // cria uma estrutura DSU para o número total de células
+
+        // processar as paredes removidas e unir as células no grafo
+        for (int i = 0; i < m; i++) {
+            int wall;     // número da parede removida
+            cin >> wall;  // lê o número da parede removida
+
+            // cálculo das posições das células baseadas na parede
+            int linha = wall / (2 * n - 1);  // 2n-1 paredes por linha
+            int num = wall % (2 * n - 1);    // para uma linha, numera colunas de 0 a 2n-2
+            if (num < n - 1) {
+                // paredes verticais
+                int esquerda = linha * n + num;
+                int direita = esquerda + 1;
+                // une as células adjacentes
+                dsu.union_sets(esquerda, direita);
+            } else {
+                // paredes horizontais
+                int cima = linha * n + (num - (n - 1));
+                int baixo = cima + n;
+                // une as células adjacentes
+                dsu.union_sets(cima, baixo);
+            }
+        }
+
+        // processar as consultas
+        for (int j = 0; j < q; j++) {
+            int a, b;  // células para verificar conectividade
+            cin >> a >> b;
+
+            // verificar se há caminho de 'a' para 'b'
+            if (dsu.find(a) == dsu.find(b)) {
+                cout << caso << "." << j << " 1\n";
+            } else {
+                cout << caso << "." << j << " 0\n";
+            }
+        }
+
+        cout << "\n";  // linha em branco entre os casos
+    }
+
+    return 0;
 }
